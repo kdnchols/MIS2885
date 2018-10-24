2416d227711b4725219cf5aa04c8f7c1b7348cfd
kdnchols <33526352+kdnchols@users.noreply.github.com>
2018-10-23 19:55:25 -0500

    Update Assignment3.java

--- JavaProgramming1/Assignment3.java
+++ JavaProgramming1/Assignment3.java
@@ -11,12 +11,12 @@ public class ShipmentDisplayLoopKevinNichols {
 		System.out.println("Enter the Shipment ID: "); //Prompts user entry
 		String shipID = scan1.nextLine(); //Reads user entry
 		
-		Scanner scan2 = new Scanner (System.in);
-		System.out.println("Enter the Shipment Weight: ");
+		
+		
 		int shipWeight = scan2.nextInt(); //Reads user entry
 		
 		Scanner scan3 = new Scanner (System.in);
-		System.out.println("Enter the Shipment Volume: ");
+		
 		int shipVolume = scan3.nextInt(); //Reads user entry
 		
 		Scanner scan4 = new Scanner (System.in);
@@ -30,30 +30,29 @@ public class ShipmentDisplayLoopKevinNichols {
 		double shipCost; //Declares shipCost variable as a double
 		shipCost = shipWeight * 10 + shipVolume * 1.5; //Calculates shipment cost
 		
-		System.out.println("Shipment ID: " + shipID); //Displays shipment's ID
-        System.out.println("Shipment Weight: " + shipWeight); //Displays shipment's weight		
-        System.out.println("Shipment Volume: " + shipVolume); //Displays shipment's volume
-		System.out.println("Shipment Origin: " + shipOrigin); //Displays shipment's origin
-        System.out.println("Shipment Destination: " + shipDestination); //Display's shipment's destination
-		System.out.println("Shipment Cost: " + shipCost); //Displays shipment cost
 		
-		int shipCount = 0;
+		
 		double processedshipTotal = 0;
 		double totalCost;
 		
-		 while (shipCount < processedshipTotal)
+		 while (true)
 		 {
+			System.out.print("Enter Shipment I.D: ");
+			 
 		    if (shipId.equalsIgnoreCase("END"))
-                break;
-            
-		    else
-		        if (shipWeight<70 && shipVolume < 120)
-		            processedshipTotals +=;
-		            totalCost = shipCost.shipcount + totalCost;
-		            shipCount++;
+               		 break;
+			 
+            	    System.out.println("Enter the Shipment Weight: ");
+			shipWeight = input.next();
+ 		System.out.println("Enter the Shipment Volume: ");
+				shipVolume = input.next();
+		
+		        if (shipWeight<70 && shipVolume < 120){
+		            processedshipTotals++;
+		            totalCost += shipWeight * 10 + shipVolume * 1.5; //Calculates shipment cost
+			}
+		           
 		            
-		        else 
-		            shipcount++
 			 
 		 }
 		
